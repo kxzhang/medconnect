aab2a64af130389dc24311771ed7689812b8fdd9
Catherine <catherine@Chuns-MacBook-Pro-2.local>
2015-05-05 17:15:04 -0400

    Fix main and most-recent, add questions

--- searchLib.js
+++ searchLib.js
@@ -49,12 +49,13 @@ categoryMap["Medical Schools"] = "./medical-schools.html";
 categoryMap["Research"] = "./research.html";
 
 // Map of author names (this.author) to their profile pages.
-var u0 = new User (0, "Bob B. Boss", "bob@boss.com", true, "9/1/12", 1);
+var u0 = new User (0, "Bob B. Boss", "bob@boss.com", true, "12/25/12", 1);
 var u1 = new User (1, "Alyssa P. Hacker", "alyssa@hacker.com", true, "8/31/10", 6);
 var u2 = new User (2, "Ben Bitdiddle", "ben@bitdiddle.com", false, "9/3/10", 6);
 var u3 = new User (3, "Carol Snow", "", false, "10/2/11", 4);
 var u4 = new User (4, "Eve Night", "eve@night.com", true, "9/12/11", 4);
-var userListing = [u0, u1, u2, u3, u4];
+var u5 = new User (5, "Mallory Mallet", "mal@mal.com", true, "8/30/10", 3)
+var userListing = [u0, u1, u2, u3, u4, u5];
 
 var questionList = [];
 	// Figure out some way to import/populate questions... 
@@ -79,7 +80,7 @@ a1 = new Answer("Because...", 2, "4/2/15", 110,
 	"you'll make all that money back once you graduate!");
 var q2 = new Question(3, "Why are Harvard/Stanford/Columbia application fees so expensive?", 3, 
 	"3/31/15", 27, "The prices are just ridiculous...  Why??", "Application Process", 
-	["applications", "stanford", "money"], [a1]);
+	["applications", "stanford", "harvard", "money"], [a1]);
 
 a1 = new Answer("My experience", 2, "9/1/14", 14,
 	"Just relax and act professionally.  I recommend bringing a small notebook " + 
@@ -132,16 +133,113 @@ var q7 = new Question(4, "Cardiology = dissection?",
 	"Do we have to dissect hearts in cardiology?  All that blood...", 
 	"Courses", ["cardiology", "dissections"], [a1]);
 
-a1 = new Answer("", 1, "1/1/13", 0, 
-	"Look it up on the Internet.");
+a1 = new Answer("", 1, "1/1/13", 100, 
+	"There's already an older question concerning this matter... look it up.");
 var q8 = new Question(7, "Plz Help!!!", 4, "1/1/13", -3, 
 	"What is the MCATs?  Plz help",
 	"MCAT Exams", ["mcat-exams"], [a1]);
 
-a1 = new Answer("", )
+a1 = new Answer("", 1, "12/30/12", 97, "Well, there are no late sign ups. " + 
+	"Your only option is to wait until the next exam, which is at " + 
+	"most 2 months away.");
+a2 = new Answer("", 3, "12/31/12", -4, "Maybe talk to your advisor?");
+var q9 = new Question(8, "MCAT Help", 2, "12/25/12", 126,
+	"I ate too much last night, went to sleep and forgot to sign up for the " + 
+	"MCAT exams, and the deadline was last night 11:59 PM.  What should I do?", 
+	"MCAT Exams", ["mcat-exams", "lateness"], [a1, a2]);
+
+a1 = new Answer("", 3, "12/28/12", 0, "From reputation alone, I think Columbia " + 
+	"is better?  Plus you get to stay in NYC, and John Hopkins just can't " + 
+	"compare to that in my opinion.  I don't know much about cardiology though.");
+a2 = new Answer("", 5, "12/31/12", -99, "lol harvard rules");
+var q10 = new Question(9, "Columbia or John Hopkins?", 2, "12/23/12", 8,
+	"While preparing for the MCAT exams, I was wondering - which medical school " + 
+	"would be better for a cardiology student, Columbia or John Hopkins?",
+	"Medical Schools", ["columbia", "john-hopkins", "cardiology"], [a1, a2]);
+
+a1 = new Answer("", 2, "12/18/12", 30, "I'd take chemistry - 5.112 in particular.  " + 
+	"Though it's more difficult, it really helps you in later, more advanced " + 
+	"pre-med classes, and integrates physics in really well.");
+var q11 = new Question(10, "GIR Question", 4, "12/18/12", 50, "Which chemistry " + 
+	"GIR would you recommend, material science (course 3) or standard " + 
+	"chemistry (course 5)?  Please give a reason.", "Courses", 
+	["classes", "chemistry", "material-science", "girs"], [a1]);
+
+a1 = new Answer("", 1, "12/17/12", 3, "Looking at the official website will " + 
+	"give a much better answer than I could, but it's a test sort of like the " + 
+	"SATs, except for medical schools.  Don't worry about it yet.");
+var q12 = new Question(11, "What are the MCATs?", 3, "12/16/12", -1, "I know " + 
+	"this is a stupid question, but what are the MCAT exams?  I keep on hearing " + 
+	"about them.", "MCAT Exams", ["mcat-exams"], [a1]);
+
+a1 = new Answer("", 2, "12/8/12", 12, "I would recommend thinking about it " + 
+	"towards the second half of your sophomore year, then really focusing on it " + 
+	"your junior year.  You should apply in the second half of your junior year.");
+a2 = new Answer("", 3, "12/2/12", 1, "I have the same question!  Could somebody " + 
+	"answer please?");
+var q13 = new Question(12, "When to apply?", 4, "12/1/12", 6, "When should I " + 
+	"start the medical school application process?  Assuming that I know for sure " + 
+	"I'm going into pre-med.", "Application Process", ["applications", "worries"],
+	[a1, a2]);
+
+a1 = new Answer("", 2, "12/8/12", 12, "I would recommend thinking about it " + 
+	"towards the second half of your sophomore year, then really focusing on it " + 
+	"your junior year.  You should apply in the second half of your junior year.");
+a2 = new Answer("", 5, "12/6/12", -1, "As soon as possible");
+var q13 = new Question(12, "When to apply?", 4, "12/1/12", 6, "When should I " + 
+	"start the medical school application process?  Assuming that I know for sure " + 
+	"I'm going into pre-med.", "Application Process", ["applications", "worries"],
+	[a1, a2]);
+
+a1 = new Answer("", 1, "11/30/12", 8, "It varies from school to schoo, but " + 
+	"in general you need 2-3 letters.  The more specific, the better.");
+var q14 = new Question(13, "How many recommendation letters do I need?", 3, 
+	"11/28/12", 20, "For college I only needed two letters, so how many " + 
+	"do I need for medical school?", "Application Process", 
+	["applications", "recommendations", "letters"], [a1]);
+
+a1 = new Answer("", 4, "11/23/12", 12, "I'd say that it's more technical " + 
+	"than the average medical school. It contains more med-engineering courses.");
+var q15 = new Question(14, "Is the HST program very technical?", 3, 
+	"11/22/12", 5, "I love the technical side of pre-med so a friend recommended the " + 
+	"HST program to me; anyone know how it is compared to the average program?", 
+	"Medical Schools", ["harvard", "hst"], [a1]);
+
+a1 = new Answer("", 3, "11/21/12", 15, "Even though it sounds cheesy, " + 
+	"I really like goint to the mock interviews the department puts on for us!  " + 
+	"They're stress-free but still helpful.");
+a2 = new Answer("", 2, "11/21/12", 13, "Keeping calm and being myself, with the " + 
+	"power of six interviewing practice books behind me.");
+var q16 = new Question(15, "Interview-related question for everyone...", 1, 
+	"11/21/12", 26, "As the interviewing season begins, what are everyone's " + 
+	"favorite tips or pieces of advice for the interviewee?", "Interviews", 
+	["interviews"], [a1, a2]);
+
+a1 = new Answer("", 1, "11/20/12", 17, "You should wear something business casual.");
+var q17 = new Question(16, "What should the dress code be?", 3, "11/20/12", 16, 
+	"I want to look serious but not overdressed.  If the school doesn't specify, " + 
+	"what should I wear to an interview?", "Interviews", ["interviews", "dress"], [a1]);
+
+a1 = new Answer("", 1, "11/17/12", 27, "Relax, you're only in your first year here!  " + 
+	"Spend some time checking out the different student activities, or try doing a " + 
+	"UROP in a different department.");
+var q18 = new Question(17, "No one wants to hire freshmen!", 5, "11/3/12", 8,
+	"Help!  I'm a freshman and I can't get a UROP at Broadhead!  What should I do??",
+	"Research", ["research", "urops", "broadhead"], [a1]);
+
+a1 = new Answer("", 2, "10/31/12", 11, "Yes; there's a template link under " + 
+	"'Resources' on the main UROP page.");
+a2 = new Answer("", 1, "10/30/12", 10, "There should be plenty of templates " + 
+	"available on the Internet; I remember writing my first one using one I " + 
+	"found with Google search.");
+var q19 = new Question(18, "Help with Writing Proposals", 3, "10/30/12", 51,
+	"I've never written a research proposal before; does anyone have any handy " + 
+	"templates or resources?", "Research", ["research", "proposals", "writing"], [a1, a2]);
+
 
 // Latest to earliest
-questionList = [q1, q2, q4, q6, q7, q5, q3, q8];
+questionList = [q1, q2, q4, q6, q7, q5, q3, q8, q9, q10, 
+q11, q12, q13, q14, q15, q16, q17, q18, q19];
 
 function searchQuestions (keyword) {
 	// General keyword search through question titles and tags.
